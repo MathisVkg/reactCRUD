c2f05567d2a5f6e9076f07a76180acaf00539ad3
Vankoekelberg <Mathis>
2022-03-29 13:42:32 +0200

    edit

---
+++ WebAPI/WebAPI/Models/DishGestionContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebAPI.Models
+{
+    public class DishGestionContext: DbContext
+    {
+        public DishGestionContext(DbContextOptions<DishGestionContext> options): base(options)
+        {
+
+        }
+
+        public DbSet<Orders> Orders { get; set; }
+    }
+}
