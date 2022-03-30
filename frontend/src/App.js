65e4fdf192b55c9adfb1709e3eb9158b68d6dbed
Vankoekelberg <Mathis>
2022-03-30 19:18:50 +0200

    edit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,14 @@
 import './App.css';
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
-import MyPopupForm from './component/popupForm';
 import { BsPencilSquare } from 'react-icons/bs';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Button, Modal, ModalBody, ModalHeader, ModalFooter } from 'reactstrap';
 
 function App() {
+  const [modal, setModal] = useState(false);
+  const toggle = () => setModal(!modal);
   const [data, setData] = useState([]);
-  let isChecked = [];
   const apiUrl = "https://localhost:7168/OrderDatabase"
 
   useEffect(async () => {
@@ -17,26 +17,6 @@ function App() {
       setData(resp.data);
     })
   }, [])
-
-  function ModalBody() {
-    return(
-     <div>
-        <Button color="success" onClick={ function noRefCheck(){} }>Ajouter +</Button>
-        <Modal centered fullscreen="md" size="" toggle={ function noRefCheck(){} }>
-            <ModalHeader toggle={ function noRefCheck(){} }>Modal title</ModalHeader>
-            <ModalBody>
-                <MyPopupForm />
-                <p>test</p>
-            </ModalBody>
-            <ModalFooter>
-                <Button color="primary" onClick={ function noRefCheck(){} }>Do Something</Button>
-                {' '}
-                <Button onClick={ function noRefCheck(){} }>Cancel</Button>
-            </ModalFooter>
-        </Modal>
-    </div> 
-    )
-  }
 
   function TableOrders() {
     return(
@@ -69,6 +49,54 @@ function App() {
     )
   }
 
+  function PopupModal() {
+    return(
+      <div>                
+          <Button color="success" onClick={ toggle }>Ajouter +</Button>
+          <Modal centered fullscreen="md" size="" isOpen={ modal } toggle={toggle}>
+              <ModalHeader>
+                <h3>Ajouter un plat</h3>
+                <button type="button" className="cross-modal btn" onClick={ toggle }>x</button>
+              </ModalHeader>
+              <ModalBody>
+                <div>
+                  <form className="d-flex flex-column">
+                      <div className="input-group flex-column mt-2">
+                        <span>Libellé du plat</span>
+                        <input className="form-control w-100" type="text" name="name" value=""/>
+                      </div>
+                      <div className="input-group flex-column mt-3">
+                        <span>Famille du plat</span>
+                        <select class="form-select w-100" name="dishfamily">
+                            <option value="1">Sandwich</option>
+                            <option value="2">Dinner</option>
+                            <option value="3">En-câs</option>
+                        </select>
+                      </div>
+                      <div className="input-group flex-column mt-3">
+                        <span>Fournisseur</span>
+                        <select class="form-select w-100" name="provider" >
+                            <option value="1">Carrefour</option>
+                            <option value="2">Lustu</option>
+                            <option value="3">Herta</option>
+                        </select>
+                      </div>
+                      <div className="input-group flex-column mt-3">
+                        <span>Prix</span>
+                        <input class="form-control w-100" type="number" min="0"/>
+                      </div>
+                  </form>
+                </div>
+              </ModalBody>
+              <ModalFooter>
+                  <Button color="ligt" className="btn-sm" onClick={ toggle }>Annuler</Button>
+                  <Button color="success" className="btn-sm">Ajouter</Button>
+              </ModalFooter>
+          </Modal>
+      </div>  
+    )
+  }
+
   function deleteOrder(event) {
     console.log(event.target.id)
   }
@@ -84,7 +112,7 @@ function App() {
       <div className="d-flex justify-content-between align-items-center containerTop">
         <h2 className="title">Gestion de plats</h2>
         <div className="d-flex ">
-          <ModalBody />
+          <PopupModal />
         </div>
       </div>
       <TableOrders />
